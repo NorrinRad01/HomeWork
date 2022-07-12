38df97d017f04dec0e5b722f391a61af0bd01ad4
NorrinRadd <Epicever94@gmail.com>
2022-07-12 13:05:39 +0300

    HM1

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2:
+int num1, num2;
+
+  Console.Write("Введите первое число: ");
+  num1 = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите второе число: ");
+  num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1 + "max");
+    Console.WriteLine(num2 + "min");
+}
+
+else if (num1 < num2)
+{
+    Console.WriteLine(num1 + " min");
+    Console.WriteLine(num2 + " max");
+} 
+
+/* Задача 4:
+
+int a, b, c;
+
+  Console.Write("Введите первое число: ");
+  a = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите второе число: ");
+  b = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите третье число: ");
+  c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+*/
