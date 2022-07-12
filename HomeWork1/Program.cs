30ee0cda065f6eb6b629916e12a8e334a19d4e74
NorrinRadd <Epicever94@gmail.com>
2022-07-12 13:42:05 +0300

    HomeWork argen

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,4 +1,4 @@
-﻿/* Задача 2:
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 int num1, num2;
 
   Console.Write("Введите первое число: ");
@@ -18,7 +18,7 @@ else if (num1 < num2)
     Console.WriteLine(num2 + " max");
 } 
 
-/* Задача 4:
+/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 int a, b, c;
 
@@ -39,3 +39,36 @@ Console.Write("max = ");
 Console.WriteLine(max);
 
 */
+
+/*
+//Задача 6:Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+bool delenie(int num)
+{
+    if(num % 2 == 0) return true;
+    else return false;
+}
+
+Console.Write("input number ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool result = delenie(number);
+
+Console.WriteLine(result);
+*/
+
+/*
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.Write("input number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+while (current <= num)
+{
+    if(current % 2 == 0)
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+
